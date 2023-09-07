18270d0eeec6f172ef5b86409a44e2bb2c2e2ed2
Shafayatul <aabir68@yahoo.com>
2023-09-07 13:37:37 +0600

    Free Mockup Product

---
+++ page-free-mockup-product.php
@@ -0,0 +1,284 @@
+<?php 
+/*
+Template Name: Free Mockup Product
+*/
+get_header();
+?>
+
+<main>
+      <section class="banner_section">
+        <div class="container">
+          
+          <!-- Banner search form -->
+          <?php include get_template_directory() .'/includes/search-form.php';?>
+
+          
+        </div>
+      </section>
+      <section class="affiliate_ad">
+        <div class="container">
+          <div class="section_width">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/affiliate_ad.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section class="free_product_section">
+        <div class="container">
+          <div class="row_d">
+            <div class="free_product_main_col left_col">
+              <div class="inner_col">
+                <div class="pixpine_free_product_top">
+                  <div class="first_free_product_container">
+                    <div class="row_d">
+                      <div class="first_image_text_col">
+                        <div class="card_item">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </div>
+                        <div class="card_item">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </div>
+                        <p>
+                          We are <a href="">PIXPINE</a> where people can get high-quality
+                          user-friendly mockups for their projects. Everything
+                          we do is for the purpose that it delivers and drives
+                          value for our customers. It’s a win-win scenario for
+                          all involved as we offer solutions to buyers’ /
+                          perspective buyers’ situations. We are a group of
+                          creative enthusiasts who work tirelessly to bring
+                          something fresh and beneficial to the market.
+                        </p>
+                        <p>
+                        It’s a win-win scenario for
+                          all <a href="">involved</a> as we offer solutions to buyers’ /
+                          perspective buyers’ situations. We are a group of
+                          creative enthusiasts who work tirelessly to bring
+                          something fresh and beneficial to the market.
+                        </p>
+                      </div>
+                      <div class="first_card_col">
+                        <div class="card_item">
+                          <a href="">
+                            <div class="inner_col">
+                              <div class="img_col pixpine_card_border">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                              </div>
+                              <div class="text_col">
+                                <h4 class="default_color">
+                                  Free Business Card with Letterhead Mockup
+                                </h4>
+                                <p class="primary_color">Premium Mockups</p>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="card_item">
+                          <a href="">
+                            <div class="inner_col">
+                              <div class="img_col pixpine_card_border">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                              </div>
+                              <div class="text_col">
+                                <h4 class="default_color">
+                                  Business Card Mockup
+                                </h4>
+                                <p class="primary_color">Premium Mockups</p>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="card_item get_off">
+                          <div class="inner_col">
+                            <div class="get_premium_mockups_section">
+                              <div
+                                class="premium_mockups_inner d-flex justify-content-between align-items-center"
+                              >
+                                <div class="premium_text text-center w-100">
+                                  <h2
+                                    class="section_heading section_heading_primary section_heading_bold"
+                                  >
+                                    Get 96% Off
+                                    <span
+                                      class="section_heading_medium default_color"
+                                      >on premium mockups</span
+                                    >
+                                  </h2>
+                                  <span class="primary_color"
+                                    >As low as $0.14 a mockup</span
+                                  >
+                                  <a
+                                    href="subscription.php"
+                                    class="btn_primary _btn"
+                                    >Get premium</a
+                                  >
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card_item">
+                          <a href="">
+                            <div class="inner_col">
+                              <div class="img_col pixpine_card_border">
+                                <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                              </div>
+                              <div class="text_col">
+                                <h4 class="default_color">
+                                  Business Card Mockup
+                                </h4>
+                                <p class="primary_color">Premium Mockups</p>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="application_photoshop_col">
+                    <div class="row_d justify-content-between">
+                      <div class="application_photoshop_content">
+                        <div class="d-flex">
+                          <div class="keys_col">
+                            <p>Application:</p>
+                            <p>File Type:</p>
+                            <p>File Size:</p>
+                            <p>Dimension:</p>
+                            <p>License:</p>
+                            <p>Author:</p>
+                          </div>
+                          <div class="values_col">
+                            <p>Photoshop</p>
+                            <p>PSD</p>
+                            <p>80 MB</p>
+                            <p>4500 x 3200 - 300dpi</p>
+                            <p><a href="license.php">Personal & Commercial</a></p>
+                            <p>Pixpine</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="application_photoshop_img google_ad">
+                        <img
+                          src="<?php echo get_template_directory_uri();?>/assets/images/application_photoshop_img.png"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <a target="_blank"
+                      class="_btn btn_primary"
+                      href="free-mockups-download-page.php"
+                      >DOWNLOAD</a
+                    >
+                  </div>
+                </div>
+                <div class="google_ad">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/google_ad_h117.png" alt="" />
+                </div>
+                <div class="related_mockups_col">
+                  <div class="section_heading_container">
+                    <h2 class="section_heading">Related Mockups</h2>
+                  </div>
+                  <div class="related_inner_col">
+                    <div class="row_d">
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                      <div class="card_item">
+                        <a href="">
+                          <div class="card_inner pixpine_card_border">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/premium_img.png" alt="" />
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="free_business_card_col right_col">
+              <div class="heading_col">
+                <h1 class="page_heading">
+                  Free Business Card with Letterhead Mockup
+                </h1>
+                <p>Business Card</p>
+              </div>
+              <div class="content_col">
+                <div class="google_add">
+                  <img
+                    id="desktop_ad"
+                    src="<?php echo get_template_directory_uri();?>/assets/images/google_ad_long_height.png"
+                    alt=""
+                  />
+                  <img id="mobile_ad" src="<?php echo get_template_directory_uri();?>/assets/images/google_ad_h117.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+<!-- Footer -->
+<?php get_footer();?>
