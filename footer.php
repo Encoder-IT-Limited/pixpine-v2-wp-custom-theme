cb6d01117d315d3e770c461bdff16ed48763aa7b
Shafayatul <aabir68@yahoo.com>
2023-09-07 13:25:57 +0600

    Bundle Mockups

--- footer.php
+++ footer.php
@@ -1,34 +1,104 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package PixPine
- */
+<!-- Log in Modal -->
+<?php include get_template_directory() .'/includes/login.php';?>
 
-?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pixpine' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'pixpine' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'pixpine' ), 'pixpine', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<!-- Signup Modal -->
+<?php include get_template_directory() .'/includes/signup.php';?>
 
-<?php wp_footer(); ?>
 
-</body>
+<footer>
+      <div class="footer_menu_col">
+        <div class="container">
+          <div class="menu_inner_col">
+            <div class="item">
+              <h3>Legal</h3>
+              <ul>
+                <li><a href="terms-and-conditions.php">Terms & Conditions</a></li>
+                <li><a href="policy.php">Privacy Policy</a></li>
+                <li><a href="license.php">Licenses</a></li>
+              </ul>
+            </div>
+            <div class="item">
+              <h3>Mockups Categories</h3>
+              <ul>
+                <li><a href="free-mockups.php">Free Mockups</a></li>
+                <li><a href="premium-mockups.php">Premium Mockup</a></li>
+                <li><a href="bundle-mockups.php">Bundle Mockups</a></li>
+              </ul>
+            </div>
+            <div class="item">
+              <h3>Support</h3>
+              <ul>
+                <li><a href="contact-us.php">Tutorials</a></li>
+                <li><a href="contact-us.php">Contact us</a></li>
+                <li><a href="subscription.php#faq">FAQ</a></li>
+              </ul>
+            </div>
+            <div class="item subscribe_col">
+              <div class="inner_col">
+                <h2>Download Unlimited Mockups</h2>
+                <p>As low as $0.14 a mockup</p>
+                <div
+                  class="btn_subscribe_cont d-flex justify-content-between align-items-center"
+                >
+                  <span>$14.00/mo</span>
+                  <a class="btn_primary _btn" href="subscription.php">
+                    Get premium
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer_last_col">
+        <div class="container">
+          <div class="last_inner_container d-flex justify-content-between">
+            <div class="copyright_text d-flex">
+              <a id="desktop_footer_logo" href="index.php">
+                <img class="footer_logo" src="<?php echo get_template_directory_uri();?>/assets/images/footer_logo.png" alt="" />
+              </a>
+              <p>
+                ©2019 - 2023 <a href="index.php">Pixpine</a> - All rights
+                reserved
+              </p>
+            </div>
+            <ul class="d-flex social_link">
+              <li>
+                <a target="_blank" href="https://www.youtube.com/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/youtube.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.pinterest.com/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/pinterest.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.instagram.com/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/instagram.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://twitter.com/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/twitter.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.behance.net/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/behance.png" alt="" />
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.facebook.com/">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/facebook.png" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
 </html>
--- header.php
+++ header.php
@@ -1,59 +1,13 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package PixPine
- */
-
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Pixpine</title>
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri();?>/assets/images/favicon.ico" />
 	<?php wp_head(); ?>
-</head>
+  </head>
+  <body>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pixpine' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$pixpine_description = get_bloginfo( 'description', 'display' );
-			if ( $pixpine_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $pixpine_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pixpine' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+<!-- Menu bar -->
+<?php include get_template_directory() .'/includes/menu.php';?>
