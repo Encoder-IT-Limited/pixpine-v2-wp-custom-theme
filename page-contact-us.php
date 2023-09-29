3c72fa4c48b8bad4a8c8cda8c9c4902cf434570c
Shafayatul <aabir68@yahoo.com>
2023-09-29 17:31:49 +0600

    contact us page done

--- page-contact-us.php
+++ page-contact-us.php
@@ -3,6 +3,42 @@
 Template Name: Contact Us
 */
 get_header();
+$msg = '';
+if(isset($_POST['c_submit'])){
+  if (isset($_POST['client_form_nonce']) && wp_verify_nonce($_POST['client_form_nonce'], 'client_form_nonce')) {
+    // Process the form data
+    $c_name = $_POST['c_name'];
+    $c_subject = $_POST['c_subject'];
+    $c_concern = $_POST['c_concern'];
+    $c_email = sanitize_email($_POST['c_email']);
+
+    // Email recipient (customize this)
+    $to = 'mdshafayatul@gmail.com'; // Replace with the client's email address
+    $subject = 'From contact us';
+
+    // Email body
+    $message = '<html><body>';
+    $message .= '<p>Name: ' . $c_name . '</p>';
+    $message .= '<p>Email: ' . $c_email . '</p>';
+    $message .= '<p>Subject: ' . $c_subject . '</p>';
+    $message .= '<p>Concern: ' . $c_concern . '</p>';
+    $message .= '</body></html>';
+
+    $headers = array(
+      'Content-Type: text/html; charset=UTF-8',
+    );
+
+    if (wp_mail($to, $subject, $message, $headers)) {
+      $msg = 'success';
+    } else {
+      $msg = 'fail';
+    }      
+  } else {
+      // Nonce verification failed, handle the error
+      wp_die('Security check failed.');
+  }
+}
+
 ?>
     <main>
       <section class="banner_section">
@@ -31,38 +67,57 @@ get_header();
                     as possible.
                   </p>
                 </div>
-                <form action="">
+                <form action="" method="post">
+
+                <?php 
+                  if($msg != ''){
+                    if($msg == 'success'){
+                      echo '
+                      <div class="alert alert-success" role="alert">
+                        Message sent successful. We will contact you ASAP.
+                      </div>
+                      ';
+                    }elseif($msg == 'fail'){
+                      echo '
+                      <div class="alert alert-warning" role="alert">
+                        Message sending failed.
+                      </div>
+                      ';
+                    }
+                  }
+                
+                ?>
+                <?php wp_nonce_field('client_form_nonce', 'client_form_nonce'); ?>
                   <div class="input_group half_width">
                     <label for="">First Name</label>
-                    <input type="text" name="" id="" required />
+                    <input type="text" name="c_name" id="" required />
                   </div>
                   <div class="input_group half_width">
                     <label for="">Email</label>
-                    <input type="email" name="" id="" required />
+                    <input type="email" name="c_email" id="" required />
                   </div>
                   <div class="input_group half_width">
                     <label for="">Choose Subject</label>
-                    <select name="" id="">
-                      <option value="general_question">General Question</option>
-                      <option value="payment_issues">Payment Issues</option>
-                      <option value="technical_support">
-                        Technical Support
-                      </option>
-                      <option value="feedback">Feedback</option>
-                      <option value="subscription">Subscription</option>
-                      <option value="download_issues">Download Issues</option>
+                    <select name="c_subject" id="" required>
+                      <option value="General question">General Question</option>
+                      <option value="Payment issues">Payment Issues</option>
+                      <option value="Technical support">Technical Support</option>
+                      <option value="Feedback">Feedback</option>
+                      <option value="Subscription">Subscription</option>
+                      <option value="Download issues">Download Issues</option>
                     </select>
                   </div>
 
                   <div class="input_group">
                     <label for="">Write your concern</label>
-                    <textarea name="" id="" cols="" rows="" required></textarea>
+                    <textarea name="c_concern" id="" cols="" rows="" required></textarea>
                   </div>
                   <div class="text-end">
                     <input
                       class="_btn btn_primary btn_helvetica"
                       type="submit"
                       value="Submit"
+                      name="c_submit"
                     />
                   </div>
                 </form>
