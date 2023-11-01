530a1fbefccf75d37d2512ea9e180d5032b58663
Shafayatul <aabir68@yahoo.com>
2023-11-02 03:09:36 +0600

    style by Harun

--- page-cart.php
+++ page-cart.php
@@ -6,59 +6,78 @@ get_header();
 ?>
 
 <main>
-
   <section class="dashboard_section dashboard__downloads">
     <div class="container">
-      <div class="row">
-        <div class="col-md-8">
-          <table>
-            <tbody>
-              <?php
-              global $wpdb;
-              $user_id = get_current_user_id();
-              $table_name = $wpdb->prefix . 'pixpine_carts';
-              $query = "SELECT product_id FROM $table_name WHERE user_id='$user_id'";
-              $products = $wpdb->get_col($query);
-              $total_price = 0;
-              foreach($products as $cpt_id){
-                $cpt_post = get_post($cpt_id, 'product');
-                $thumbnail_url = get_the_post_thumbnail_url($cpt_id);
-                $price = get_post_meta($cpt_id, 'personal_commercial_price', true);
-                if(empty($price)){
-                  $price = 0;
-                }
-                $total_price += $price;
-              ?>
-                <tr id="<?php echo $cpt_id;?>">
-                  <td><img style="width: 100px" src="<?php echo $thumbnail_url;?>" alt=""></td>
-                  <td>
-                    <b><?php echo $cpt_post->post_title;?></b>  
-                    ID: <?php echo $cpt_id;?>
-                  </td>
-                  <td> $<span class="single-cart-price"><?php echo $price;?></span> </td>
-                  <td class="remove-cart" p-id="<?php echo $cpt_id;?>" ><img style="width: 20px" src="<?php echo get_template_directory_uri();?>/assets/images/remove.png" alt="" /></td>
-                </tr>
-              <?php
-              }
-              ?>
-
-            
-            </tbody>
-          </table>
+      <div class="row_d cart__page">
+        <div class="mockups_cart_item_column">
+          <div class="table__cart">
+            <?php
+				  global $wpdb;
+				  $user_id = get_current_user_id();
+				  $table_name = $wpdb->prefix . 'pixpine_carts'; $query = "SELECT product_id
+            FROM $table_name WHERE user_id='$user_id'"; $products =
+            $wpdb->get_col($query); $total_price = 0; foreach($products as
+            $cpt_id){ $cpt_post = get_post($cpt_id, 'product'); $thumbnail_url =
+            get_the_post_thumbnail_url($cpt_id); $price = get_post_meta($cpt_id,
+            'personal_commercial_price', true); if(empty($price)){ $price = 0; }
+            $total_price += $price; ?>
+            <div class="cart_item" id="<?php echo $cpt_id;?>">
+              <div class="img_text_cont">
+                <div class="product_img">
+                  <img src="<?php echo $thumbnail_url;?>" alt="" />
+                </div>
+                <div class="product_description">
+                  <span class="product_name">
+					  <strong><?php echo $cpt_post->post_title;?></strong>
+					</span>
+                  <span>
+					  ID: <?php echo $cpt_id;?>
+					</span>
+                </div>
+              </div>
+              <div class="">
+                $<span class="single-cart-price"><?php echo $price;?></span>
+              </div>
+              <div class="remove-cart" p-id="<?php echo $cpt_id;?>">
+                <img
+                  style="width: 20px"
+                  src="<?php echo get_template_directory_uri();?>/assets/images/remove.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <?php
+				  }
+				  ?>
+          </div>
         </div>
-        <div class="col-md-4">
-          <p>CART TOTALS</p>
-          <p>Cart Subtotal	$<span class="total-cart-price"><?php echo $total_price;?></span></p>
-          <p>Order Total	$<span class="total-cart-price"><?php echo $total_price;?></span></p>
-
-          <p>
-            All prices are in US dollars
-            <br>
-            <a class="_btn btn_primary" href="<?php echo site_url('checkout');?>">
-              Proceed to checkout  
+        <div class="checkout_cart_btn_column">
+          <div class="inner_col">
+            <p><strong>CART TOTALS</strong></p>
+            <div class="item">
+              <p>Cart Subtotal</p>
+              <p>
+                $<span class="total-cart-price"
+                  ><?php echo $total_price;?></span
+                >
+              </p>
+            </div>
+            <div class="item">
+              <p>Order Total</p>
+              <p>
+                $<span class="total-cart-price"
+                  ><?php echo $total_price;?></span
+                >
+              </p>
+            </div>
+            <p class="text-center">All prices are in US dollars</p>
+            <a
+              class="_btn btn_primary"
+              href="<?php echo site_url('checkout');?>"
+            >
+              Proceed to checkout
             </a>
-          </p>
-
+          </div>
         </div>
       </div>
     </div>
--- page-checkout.php
+++ page-checkout.php
@@ -45,190 +45,285 @@ $billing_zip = get_user_meta($user_id, 'billing_zip', true);
 ?>
 
 <main>
-
   <section class="dashboard_section dashboard__downloads">
-    <h1 class="text-center">Checkout</h1>
-    <br>
     <div class="container">
-      <div class="row">
-        <div class="col-md-8">
+      <div class="heading_col mb-5">
+        <h1 class="page_heading text-center">Checkout</h1>
+      </div>
+      <div class="row_d checkout_row">
+        <div class="left_col">
           <?php
             if($msg != ''){
               if($msg == 'success'){
                 echo '
                 <div class="alert alert-success" role="alert">
-                  Billing address updated successful.
-                </div>
-                ';
-              }elseif($msg == 'fail'){
-                echo '
-                <div class="alert alert-warning" role="alert">
-                  Failed!! Try again later.
-                </div>
-                ';
-              }
-            }
-          ?>     
-          <div class="content__column billing_form d-none" >
-            <form action="#" method="post">
-
-              <?php wp_nonce_field('client_form_nonce', 'client_form_nonce'); ?>
-              <div class="full_width_container">
-                <div class="half_width input_group">
-                  <label for="">First Name<span>*</span></label>
-                  <input type="text" name="billing_f_name" value="<?php echo $billing_f_name;?>" id="" required>
-                </div>
-                <div class="half_width input_group">
-                  <label for="">Last Name<span>*</span></label>
-                  <input type="text" name="billing_l_name" value="<?php echo $billing_l_name;?>" id="" required>
-                </div>
+          Billing address updated successful.
+        </div>
+        '; }elseif($msg == 'fail'){ echo '
+        <div class="alert alert-warning" role="alert">
+          Failed!! Try again later.
+        </div>
+        '; } } ?>
+        <div class="content__column billing_form d-none">
+          <form action="#" method="post">
+            <?php wp_nonce_field('client_form_nonce', 'client_form_nonce'); ?>
+            <div class="full_width_container">
+              <div class="half_width input_group">
+                <label for="">First Name<span>*</span></label>
+                <input
+                  type="text"
+                  name="billing_f_name"
+                  value="<?php echo $billing_f_name;?>"
+                  id=""
+                  required
+                />
               </div>
-              <div class="full_width_container">
-                <div class="half_width input_group">
-                  <label for="">Email<span>*</span></label>
-                  <input type="email" name="billing_email" value="<?php echo $billing_email;?>" id="" required>
-                </div>
+              <div class="half_width input_group">
+                <label for="">Last Name<span>*</span></label>
+                <input
+                  type="text"
+                  name="billing_l_name"
+                  value="<?php echo $billing_l_name;?>"
+                  id=""
+                  required
+                />
               </div>
-              <div class="full_width_container">
-                <div class="half_width input_group">
-                  <label for="">Company</label>
-                  <input type="text" name="billing_company" value="<?php echo $billing_company;?>" id="">
-                </div>
-                <div class="half_width input_group">
-                  <label for="">Country<span>*</span></label>
-                  <input type="text" name="billing_country" value="<?php echo $billing_country;?>" id="" required>
-                </div>
+            </div>
+            <div class="full_width_container">
+              <div class="half_width input_group">
+                <label for="">Email<span>*</span></label>
+                <input
+                  type="email"
+                  name="billing_email"
+                  value="<?php echo $billing_email;?>"
+                  id=""
+                  required
+                />
               </div>
-              <div class="full_width_container">
-                <div class="full_width input_group">
-                  <label for="">Address</label>
-                  <input type="text" name="billing_address" value="<?php echo $billing_address;?>" id="">
-                </div>
+            </div>
+            <div class="full_width_container">
+              <div class="half_width input_group">
+                <label for="">Company</label>
+                <input
+                  type="text"
+                  name="billing_company"
+                  value="<?php echo $billing_company;?>"
+                  id=""
+                />
               </div>
-              <div class="full_width_container">
-                <div class="half_width input_group">
-                  <label for="">City</label>
-                  <input type="text" name="billing_city" value="<?php echo $billing_city;?>" id="">
-                </div>
-                <div class="half_width input_group">
-                  <label for="">State/Province/Region</label>
-                  <input type="text" name="billing_state" value="<?php echo $billing_state;?>" id="">
-                </div>
+              <div class="half_width input_group">
+                <label for="">Country<span>*</span></label>
+                <input
+                  type="text"
+                  name="billing_country"
+                  value="<?php echo $billing_country;?>"
+                  id=""
+                  required
+                />
               </div>
-              <div class="full_width_container">
-                <div class="half_width input_group">
-                  <label for="">Zip Code</label>
-                  <input type="text" name="billing_zip" value="<?php echo $billing_zip;?>" id="">
-                </div>
+            </div>
+            <div class="full_width_container">
+              <div class="full_width input_group">
+                <label for="">Address</label>
+                <input
+                  type="text"
+                  name="billing_address"
+                  value="<?php echo $billing_address;?>"
+                  id=""
+                />
               </div>
-              <div class="form_btn_container">
-                <input class="_btn btn_primary" type="submit" name="submit" value="Update">
+            </div>
+            <div class="full_width_container">
+              <div class="half_width input_group">
+                <label for="">City</label>
+                <input
+                  type="text"
+                  name="billing_city"
+                  value="<?php echo $billing_city;?>"
+                  id=""
+                />
               </div>
-            </form>
-          </div>
-
-          <p class="billing_info">
-            First Name: <?php echo $billing_f_name;?><br>
-            Last Name: <?php echo $billing_l_name;?><br>
-            Email Address: <?php echo $billing_email;?><br>
-            Country: <?php echo $billing_country;?><br>         
-          </p>
-          <button class="_btn get_premium_btn btn_black_small btn_primary show_billing_form">
-            Edit Billing Address
-          </button>
+              <div class="half_width input_group">
+                <label for="">State/Province/Region</label>
+                <input
+                  type="text"
+                  name="billing_state"
+                  value="<?php echo $billing_state;?>"
+                  id=""
+                />
+              </div>
+            </div>
+            <div class="full_width_container">
+              <div class="half_width input_group">
+                <label for="">Zip Code</label>
+                <input
+                  type="text"
+                  name="billing_zip"
+                  value="<?php echo $billing_zip;?>"
+                  id=""
+                />
+              </div>
+            </div>
+            <div class="form_btn_container">
+              <input
+                class="_btn btn_primary"
+                type="submit"
+                name="submit"
+                value="Update"
+              />
+            </div>
+          </form>
         </div>
 
-        
-        <div class="col-md-4">
+        <div class="billing_info">
+          <p>
+            First Name:
+            <?php echo $billing_f_name;?>
+          </p>
+          <p>
+            Last Name:
+            <?php echo $billing_l_name;?>
+          </p>
+          <p>
+            Email Address:
+            <?php echo $billing_email;?>
+          </p>
+          <p>
+            Country:
+            <?php echo $billing_country;?>
+          </p>
+        </div>
+        <button
+          class="_btn get_premium_btn btn_black_small btn_primary show_billing_form"
+        >
+          Edit Billing Address
+        </button>
+      </div>
+
+      <div class="right_col">
+        <div class="order__column">
           <h3 id="order_review_heading">Your order</h3>
-          <table class="shop_table woocommerce-checkout-review-order-table">
-            <thead>
-                <tr>
-                    <th class="product-name">Product</th>
-                    <th class="product-total">Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+          <div class="shop_table woocommerce-checkout-review-order-table">
+            <div
+              class="order_heading_col d-flex align-items-center justify-content-between"
+            >
+              <p class="product-name">Product</p>
+              <p class="product-total">Total</p>
+            </div>
+            <div>
+              <?php
                 global $wpdb;
                 $user_id = get_current_user_id();
-                $table_name = $wpdb->prefix . 'pixpine_carts';
-                $query = "SELECT product_id FROM $table_name WHERE user_id='$user_id'";
-                $products = $wpdb->get_col($query);
-                $total_price = 0;
-                foreach($products as $cpt_id){
-                  $cpt_post = get_post($cpt_id, 'product');
-                  $thumbnail_url = get_the_post_thumbnail_url($cpt_id);
-                  $price = get_post_meta($cpt_id, 'personal_commercial_price', true);
-                  if(empty($price)){
-                    $price = 0;
-                  }
-                  $total_price += $price;
-                ?>
-                <tr class="cart_item">
-                    <td class="product-name">
-                        <span class="product-title"><?php echo $cpt_post->post_title;?></span>
-                        <dl class="variation">
-                            <dt class="variation-ID">ID:</dt>
-                            <dd class="variation-ID">
-                                <p><?php echo $cpt_id;?></p>
-                            </dd>
-                            <dt class="variation-Dimensions">Dimensions:</dt>
-                            <dd class="variation-Dimensions">
-                                <p>6000x6000px</p>
-                            </dd>
-                            <dt class="variation-Filetype">File type:</dt>
-                            <dd class="variation-Filetype">
-                                <p>PSD Mockup</p>
-                            </dd>
-                            <!-- <dt class="variation-License">License:</dt>
+                $table_name = $wpdb->prefix . 'pixpine_carts'; $query = "SELECT
+              product_id FROM $table_name WHERE user_id='$user_id'"; $products =
+              $wpdb->get_col($query); $total_price = 0; foreach($products as
+              $cpt_id){ $cpt_post = get_post($cpt_id, 'product'); $thumbnail_url
+              = get_the_post_thumbnail_url($cpt_id); $price =
+              get_post_meta($cpt_id, 'personal_commercial_price', true);
+              if(empty($price)){ $price = 0; } $total_price += $price; ?>
+              <div class="cart_item">
+                <div class="product-name">
+                  <h5 class="product-title">
+                    <?php echo $cpt_post->post_title;?>
+                  </h5>
+                  <div class="variation">
+                    <div class="d-flex item">
+                      <span class="variation-ID">ID:</span>
+                      <span class="variation-ID">
+                        <p><?php echo $cpt_id;?></p>
+                      </span>
+                    </div>
+                    <span class="variation-Dimensions">Dimensions:</span>
+                    <div class="variation-Dimensions">
+                      <p>6000x6000px</p>
+                    </div>
+                    <div class="d-flex item">
+                      <span class="variation-Filetype">File type:</span>
+                      <span class="variation-Filetype">
+                        <p>PSD Mockup</p>
+                      </span>
+                    </div>
+                    <!-- <dt class="variation-License">License:</dt>
                             <dd class="variation-License">
                                 <p>Standard License</p>
                             </dd> -->
-                        </dl>
-                    </td>
-                    <td class="product-total">
-                        <span class="amount">$<?php echo $price;?></span>
-                    </td>
-                </tr>
-                <?php
+                  </div>
+                </div>
+                <div class="product-total">
+                  <span class="amount">$<?php echo $price;?></span>
+                </div>
+              </div>
+              <?php
                 }
                 ?>
-            </tbody>
-            <tfoot>
-                <tr class="cart-subtotal">
-                    <th>Cart Subtotal</th>
-                    <td><span class="amount">$<?php echo $total_price;?></span></td>
-                </tr>
-                <tr class="tax-rate tax-rate-de-vat-1">
-                    <th>VAT</th>
-                    <td><span class="amount">$0.00</span></td>
-                </tr>
-                <tr class="order-total order-total-main" data-total="17.84" data-total-feeless="17.84" data-total-blue="14.99">
-                    <th>Order Total</th>
-                    <td><span class="amount">$1<?php echo $total_price;?></span></td>
-                </tr>
-            </tfoot>
-          </table>
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-              <input type="hidden" name="cmd" value="_s-xclick">
-              <input type="hidden" name="hosted_button_id" value="YOUR_BUTTON_ID">
-              <!-- Specify the item name and price -->
-              <input type="hidden" name="item_name" value="Your Product Name">
-              <input type="hidden" name="amount" value="10.00">
-              <!-- Customize the button image -->
-              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="Pay with PayPal">
-              <!-- Add a return URL after a successful payment -->
-              <input type="hidden" name="return" value="https://yourwebsite.com/success.php">
-              <!-- Add a cancel URL if the user cancels the payment -->
-              <input type="hidden" name="cancel_return" value="https://yourwebsite.com/cancel.php">
-              <!-- Specify the currency code -->
-              <input type="hidden" name="currency_code" value="USD">
-              <!-- Additional optional fields -->
-              <input type="hidden" name="custom" value="Your Custom Data">
-              <input type="hidden" name="tax" value="0.00">
+            </div>
+            <div class="order_footer">
+              <div class="cart-subtotal">
+                <div class="d-flex align-items-center justify-content-between">
+                  <p>Cart Subtotal</p>
+                  <span class="amount">$<?php echo $total_price;?></span>
+                </div>
+              </div>
+              <div class="tax-rate tax-rate-de-vat-1">
+                <div class="d-flex align-items-center justify-content-between">
+                  <p>VAT</p>
+                  <span class="amount">$0.00</span>
+                </div>
+              </div>
+              <div
+                class="order-total order-total-main"
+                data-total="17.84"
+                data-total-feeless="17.84"
+                data-total-blue="14.99"
+              >
+                <div class="d-flex align-items-center justify-content-between">
+                  <p>Order Total</p>
+                  <span class="amount">$1<?php echo $total_price;?></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <form
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_top"
+          >
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input
+              type="hidden"
+              name="hosted_button_id"
+              value="YOUR_BUTTON_ID"
+            />
+            <!-- Specify the item name and price -->
+            <input type="hidden" name="item_name" value="Your Product Name" />
+            <input type="hidden" name="amount" value="10.00" />
+            <!-- Customize the button image -->
+            <input
+              class=""
+              type="image"
+              src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+              border="0"
+              name="submit"
+              alt="Pay with PayPal"
+            />
+            <!-- Add a return URL after a successful payment -->
+            <input
+              type="hidden"
+              name="return"
+              value="https://yourwebsite.com/success.php"
+            />
+            <!-- Add a cancel URL if the user cancels the payment -->
+            <input
+              type="hidden"
+              name="cancel_return"
+              value="https://yourwebsite.com/cancel.php"
+            />
+            <!-- Specify the currency code -->
+            <input type="hidden" name="currency_code" value="USD" />
+            <!-- Additional optional fields -->
+            <input type="hidden" name="custom" value="Your Custom Data" />
+            <input type="hidden" name="tax" value="0.00" />
           </form>
-
         </div>
       </div>
     </div>
