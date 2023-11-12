1f4d54f1b1da39a1ea41b1b4258fe9862425f4d0
Shafayatul <aabir68@yahoo.com>
2023-11-12 10:11:28 +0600

    welcome email done

--- functions/email-templates.php
+++ functions/email-templates.php
@@ -14,7 +14,7 @@ function pixpine_welcome_email($name){
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Profile Setup</title>
-    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="' . get_template_directory_uri() . 'styles/style.css" />
     <style>
       @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 
