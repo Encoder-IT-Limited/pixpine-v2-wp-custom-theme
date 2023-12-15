5ea7d299417296c060414a221000f2c8d1a72cfb
Shafayatul <aabir68@yahoo.com>
2023-12-16 00:52:25 +0600

    email fix

--- functions/user.php
+++ functions/user.php
@@ -130,7 +130,7 @@ function pixpine_signup(){
             // echo 'Registration successful. User ID: ' . $user_id;
             // send welcome email
             $html = pixpine_welcome_email($username);
-            ($email, 'Welcome to Pixpine', $html);
+            pixpine_send_html_email($email, 'Welcome to Pixpine', $html);
 
             $html = pixpine_new_account_password_email($password);
             pixpine_send_html_email($email, 'Pixpine - New account password', $html);
