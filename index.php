be6bdbb2fcde39b80aeeaf18ae0bfb066c9627e3
Shafayatul <aabir68@yahoo.com>
2024-01-29 12:09:49 +0600

    get_user_specific_discount

--- index.php
+++ index.php
@@ -416,7 +416,7 @@
                   <?php
                   $args = array(
                     // 'post_type' => 'product', // Replace with the name of your CPT
-                    'posts_per_page' => 10, // Number of posts to display (adjust as needed)
+                    'posts_per_page' => 12, // Number of posts to display (adjust as needed)
                     'orderby'=>'ID',
                     'order' => 'DESC', // Sorting order (DESC for latest first, ASC for oldest first)
                     'tax_query' => array(
